0a79789d7fbaee621f8aa34d806661862edc6825
Abdul Malik <141676888+JustNotSec@users.noreply.github.com>
2023-08-18 04:24:44 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+const readline = require('readline');
+const axios = require('axios');
+const gradient = require('gradient-string');
+
+const banner = `
+
+
+┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+│                                                                                                                 │
+│ ███████ ███████ ██████  ██    ██ ███████ ██████         ██████ ██   ██ ███████  ██████ ██   ██ ███████ ██████   │
+│ ██      ██      ██   ██ ██    ██ ██      ██   ██       ██      ██   ██ ██      ██      ██  ██  ██      ██   ██  │
+│ ███████ █████   ██████  ██    ██ █████   ██████  █████ ██      ███████ █████   ██      █████   █████   ██████   │
+│      ██ ██      ██   ██  ██  ██  ██      ██   ██       ██      ██   ██ ██      ██      ██  ██  ██      ██   ██  │
+│ ███████ ███████ ██   ██   ████   ███████ ██   ██        ██████ ██   ██ ███████  ██████ ██   ██ ███████ ██   ██  │
+│                                               FOR FIVEM!!!                                                      │
+│                                                                                                                 │
+└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+                           ======== AUTHOR NotSec_ | Version V1 (Stable) ========                                                                                                           
+`
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function fetchAndDisplay(url, filename) {
+    console.log(gradient.passion('Sedang Memproses Mohon Tunggu...'));
+  
+    axios.get(url)
+      .then(response => {
+        const resultPath = `result/${filename}`;
+        const responseData = JSON.stringify(response.data, null, 2);
+        fs.writeFileSync(resultPath, responseData);
+        setTimeout(() => {
+          console.clear(); // Menghapus pesan "Sedang Memproses Mohon Tunggu..."
+          console.log(gradient.passion(responseData));
+          console.log(gradient.summer('Data Ditemukan!!')); // Menampilkan pesan "Data Ditemukan!!"
+          console.log(`Isi website telah ditulis ke dalam file ${resultPath}`);
+        }, 15000); // Tunggu 15 detik sebelum menampilkan pesan "Data Ditemukan!!"
+      })
+      .catch(error => {
+        console.error('Error fetching website:', error.message);
+      })
+      .finally(() => {
+        rl.close();
+      });
+  }
+
+function handleMenuChoice(choice) {
+  switch (choice) {
+    case '1':
+      rl.question('Masukkan URL website untuk Cek Info: ', function(url) {
+        const infoUrl = `${url}/info.json`;
+        rl.question('Masukkan Nama Server: ', function(namaServer) {
+          const filename = `result_info_${namaServer}.txt`;
+          fetchAndDisplay(infoUrl, filename);
+        });
+      });
+      break;
+    case '2':
+      rl.question('Masukkan URL website untuk Cek Players: ', function(url) {
+        const playersUrl = `${url}/player.json`;
+        rl.question('Masukkan Nama Server: ', function(namaServer) {
+          const filename = `result_players_${namaServer}.txt`;
+          fetchAndDisplay(playersUrl, filename);
+        });
+      });
+      break;
+    case '3':
+      rl.question('Masukkan URL website untuk Cek Dynamic: ', function(url) {
+        const dynamicUrl = `${url}/dynamic.json`;
+        rl.question('Masukkan Nama Server: ', function(namaServer) {
+          const filename = `result_dynamic_${namaServer}.txt`;
+          fetchAndDisplay(dynamicUrl, filename);
+        });
+      });
+      break;
+    default:
+      console.log('Pilihan tidak valid.');
+      rl.close();
+      break;
+  }
+}
+console.clear()
+console.log(gradient.passion(banner))
+console.log('Pilih Metode Nya Ngab:');
+console.log('>> 1. Cek Info | Mengecek Info Dari Server');
+console.log('>> 2. Cek Players | Mengecek Info Player Dari Server');
+console.log('>> 3. Cek Dynamic | Mengecek Dynamic Info Dari Server');
+console.log('\n╔═══════(root@server)')
+
+rl.question('╚═══⮞ Pilih nomor (1/2/3)~: ', function(choice) {
+  handleMenuChoice(choice);
+});
